407391cc950b93efad414625aa5e4c9a7033e35e
roboryantron <roboryantron@gmail.com>
2017-10-30 01:30:30 -0400

    Example for prefab protection using ISerializationCallbackReceiver

---
+++ Assets/PrefabApplyProtection/Code/KeyboardMover.cs
@@ -0,0 +1,62 @@
+﻿// ----------------------------------------------------------------------------
+// Quick Tips
+// 
+// Author: Ryan Hipple
+// Date:   10/30/17
+// ----------------------------------------------------------------------------
+
+using System;
+using UnityEngine;
+
+namespace RoboRyanTron.PrefabApplyProtection
+{
+    /// <summary>
+    /// Simple class to move a game object based on keyboard input.
+    /// </summary>
+    public class KeyboardMover : MonoBehaviour
+    {
+        /// <summary>
+        /// Defines a movement axis, consisting of the keyboard input needed 
+        /// for positive and negative movement on the axis.
+        /// </summary>
+        [Serializable]
+        public class MoveAxis
+        {
+            [Tooltip("Key to move forward on this axis.")]
+            public KeyCode Positive;
+
+            [Tooltip("Key to move backward on this axis.")]
+            public KeyCode Negative;
+
+            public MoveAxis(KeyCode positive, KeyCode negative)
+            {
+                Positive = positive;
+                Negative = negative;
+            }
+            
+            public static implicit operator float(MoveAxis axis)
+            {
+                return (Input.GetKey(axis.Positive)
+                    ? 1.0f : 0.0f) -
+                    (Input.GetKey(axis.Negative)
+                    ? 1.0f : 0.0f);
+            }
+        }
+
+        [Tooltip("Units per second to move.")]
+        public float MoveRate = 1.0f;
+
+        [Tooltip("Keys to move along the horizontal axis.")]
+        public MoveAxis Horizontal = new MoveAxis(KeyCode.D, KeyCode.A);
+
+        [Tooltip("Keys to move along the vertical axis.")]
+        public MoveAxis Vertical = new MoveAxis(KeyCode.W, KeyCode.S);
+
+        private void Update()
+        {
+            Vector3 moveNormal = new Vector3(Horizontal, Vertical, 0.0f).normalized;
+
+            transform.position += moveNormal*Time.deltaTime*MoveRate;
+        }
+    }
+}
---
+++ Assets/PrefabApplyProtection/Code/Portal.cs
@@ -0,0 +1,82 @@
+﻿// ----------------------------------------------------------------------------
+// Quick Tips
+// 
+// Author: Ryan Hipple
+// Date:   10/30/17
+// ----------------------------------------------------------------------------
+
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RoboRyanTron.PrefabApplyProtection
+{
+    public class Portal : MonoBehaviour, ISerializationCallbackReceiver
+    {
+        #region -- Inspector Fields -------------------------------------------
+#if UNITY_EDITOR
+        [Tooltip("The scene to load into when the portal is entered.")]
+        public UnityEditor.SceneAsset DestinationScene;
+#endif
+        [Tooltip("The string name of the scene used by Unity to load it " +
+                 "since SceneAsset can not be saved in builds. This can't " +
+                 "be edited directly, it is derived from DestinationScene")]
+        public string DestinationSceneName;
+        #endregion -- Inspector Fields ----------------------------------------
+
+        /// <summary>
+        /// Caches the destination scene name that will be used for loading 
+        /// since Unity can not reference a SceneAsset in a build.
+        /// </summary>
+        private void OnValidate()
+        {
+            if (DestinationScene != null)
+                DestinationSceneName = DestinationScene.name;
+            else
+                DestinationSceneName = string.Empty;
+        }
+
+        /// <summary> Triggers usage of the portal. </summary>
+        /// <param name="col">Collider that entered the portal.</param>
+        private void OnTriggerEnter(Collider col)
+        {
+            Use();
+        }
+
+        /// <summary>
+        /// Load the destination scene if it is valid.
+        /// </summary>
+        public void Use()
+        {
+            if (!string.IsNullOrEmpty(DestinationSceneName))
+            {
+                SceneManager.LoadScene(DestinationSceneName);
+            }
+            else
+            {
+                throw new Exception("No valid scene specified as " +
+                                    "portal destination.");
+            }
+        }
+
+        #region -- ISerializationCallbackReceiver -----------------------------
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+#if UNITY_EDITOR
+            // Prevent saving if this is a prefab
+            if (UnityEditor.PrefabUtility.GetPrefabType(this) == 
+                UnityEditor.PrefabType.Prefab)
+            {
+                DestinationScene = null;
+                DestinationSceneName = string.Empty;
+            }
+#endif
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            // Nothing needed here
+        }
+        #endregion -- ISerializationCallbackReceiver --------------------------
+    }
+}
